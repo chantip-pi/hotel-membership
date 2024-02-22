b4064d0b82c929b11895b7dd3b4b3d50af6d2e28
himechi <chantip.pi@ku.th>
2024-02-22 23:35:00 +0700

    set up theme and add routes

---
+++ lib/views/sign_in.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:project/theme.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  void _signIn() {
+    String email = emailController.text;
+    String password = passwordController.text;
+    // Perform sign-in logic here
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.backgroundColor,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 60),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildSignInText(),
+              _buildEmailTextField(),
+              _buildPasswordTextField(),
+              _buildSignInButton(),
+              _buildSignUpPrompt(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInText() {
+    return const Text(
+      "Sign in",
+      style: TextStyle(
+        fontWeight: FontWeight.w900,
+        color: Colors.black,
+        fontSize: 30,
+      ),
+    );
+  }
+
+  Widget _buildEmailTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: TextField(
+        controller: emailController,
+        decoration: InputDecoration(
+          labelText: 'Email Address',
+          hintText: 'helloworld@gmail.com',
+          hintStyle: TextStyle(color: Colors.grey),
+          labelStyle: TextStyle(fontSize: 18, color: Colors.black),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          contentPadding: EdgeInsets.fromLTRB(12, 20, 12, 12),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppTheme.primaryColor,
+              width: 2.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: TextField(
+        controller: passwordController,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          labelStyle: TextStyle(fontSize: 18, color: Colors.black),
+          suffixIcon: GestureDetector(
+            onTap: _togglePasswordVisibility,
+            child: Icon(
+              _obscureText ? Icons.visibility : Icons.visibility_off,
+              color: Colors.grey,
+            ),
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          contentPadding: const EdgeInsets.fromLTRB(12, 20, 12, 12),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppTheme.primaryColor,
+              width: 2.0,
+            ),
+          ),
+        ),
+        obscureText: _obscureText,
+      ),
+    );
+  }
+
+  Widget _buildSignInButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(AppTheme.primaryColor),
+                fixedSize: MaterialStateProperty.all<Size>(
+                  const Size.fromHeight(56),
+                ),
+              ),
+              onPressed: _signIn,
+              child: const Center(
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignUpPrompt() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don’t have an account? "),
+        Text(
+          "Sign up",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
