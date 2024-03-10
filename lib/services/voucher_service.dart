d749c2390027a93132bf60a7d8e5c41f1b9eb38d
himechi <chantip.pi@ku.th>
2024-03-10 16:41:26 +0700

    add voucher service

---
+++ lib/services/voucher_service.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final CollectionReference vouchers = FirebaseFirestore.instance.collection('vouchers');
+
+  // Create
+  Future<void> addVoucher({
+    required String name,
+    required bool onShop,
+    required Timestamp timestamp,
+    required int points,
+    required Timestamp dueDate,
+    required String voucherType,
+    required String termsCondition,
+    double? discountPercentage,
+    double? cashValue,
+    String? giftItem,
+  }) {
+    return vouchers.add({
+      'name': name,
+      'onShop': onShop,
+      'timestamp': timestamp,
+      'points': points,
+      'dueDate': dueDate,
+      'voucherType': voucherType,
+      'termsCondition': termsCondition,
+      'discountPercentage': discountPercentage,
+      'cashValue': cashValue,
+      'giftItem': giftItem,
+    });
+  }
+
+  // Read
+  Stream<QuerySnapshot> getVoucherStream() {
+    final voucherStream = vouchers.orderBy('name', descending: false).snapshots();
+    return voucherStream;
+  }
+
+  // Update
+  Future<void> deleteVoucher(String docID) {
+    return vouchers.doc(docID).update({
+      'onShop': false,
+      'timestamp': Timestamp.now(),
+    });
+  }
+}
