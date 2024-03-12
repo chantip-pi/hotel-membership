0367483d4a49d9d1c18e66f2865b1925eb5d82cf
himechi <chantip.pi@ku.th>
2024-03-12 18:26:36 +0700

    add light gold color

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,6 @@ class AppTheme {
   static const Color backgroundColor = Color(0xFFF7F7F7);
   static const Color primaryColor = Color.fromARGB(255, 167, 106, 9);
   static const Color secondaryColor = Color.fromARGB(144, 167, 106, 9);
-
+  static const Color lightGoldColor = Color.fromARGB(205, 167, 106, 9);
 }
 
