cd19518daeb3e39e8b340015382299326f7ed3d8
himechi <chantip.pi@ku.th>
2024-03-14 18:52:35 +0700

    add my voucher function for user

--- lib/services/user_purchase.dart
+++ lib/services/user_purchase.dart
@@ -1,27 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/services/voucher_service.dart';
 
-class UserPurchaseService{
-  final CollectionReference purchases = FirebaseFirestore.instance.collection('userPurchase');
-  final CollectionReference vouchers = FirebaseFirestore.instance.collection('vouchers');
+class UserPurchaseService {
+  final CollectionReference purchases =
+      FirebaseFirestore.instance.collection('userPurchase');
+  final CollectionReference vouchers =
+      FirebaseFirestore.instance.collection('vouchers');
 
   // Create
-  Future<void> addPurchase({
-    required String userID,
-    required String voucherID,
-    required bool isRedeem
-  }) {
+  Future<void> addPurchase(
+      {required String userID,
+      required String voucherID,
+      required bool isRedeem}) {
     return purchases.add({
       'userID': userID,
       'voucherID': voucherID,
       'timestamp': Timestamp.now(),
-      'isRedeem' : false
+      'isRedeem': false
     });
   }
 
   // Read
   Stream<QuerySnapshot> getPurchaseStream() {
-    final voucherStream = purchases.orderBy('timestamp', descending: true).snapshots();
+    final voucherStream =
+        purchases.orderBy('timestamp', descending: true).snapshots();
     return voucherStream;
   }
 
@@ -33,12 +35,11 @@ class UserPurchaseService{
     });
   }
 
- Future<List<String>> getAllVoucherIDsForUser(String userID) async {
+  Future<List<String>> getAllVoucherIDsForUser(String userID) async {
     try {
       // Querying userPurchases collection to find all purchases made by the user
-      QuerySnapshot userPurchaseSnapshot = await purchases
-          .where('userID', isEqualTo: userID)
-          .get();
+      QuerySnapshot userPurchaseSnapshot =
+          await purchases.where('userID', isEqualTo: userID).get();
       // Extracting voucher IDs from user purchases
       List<String> voucherIDs = [];
       userPurchaseSnapshot.docs.forEach((doc) {
@@ -53,51 +54,114 @@ class UserPurchaseService{
     }
   }
 
-Stream<QuerySnapshot> getVoucherStreamByIDs(List<String> voucherIDs) {
-  final voucherStream = FirebaseFirestore.instance
-      .collection('vouchers')
-      .where(FieldPath.documentId, whereIn: voucherIDs)
-      .snapshots();
+  Stream<QuerySnapshot> getVoucherStreamByIDs(List<String> voucherIDs) {
+    final voucherStream = FirebaseFirestore.instance
+        .collection('vouchers')
+        .where(FieldPath.documentId, whereIn: voucherIDs)
+        .snapshots();
 
-  voucherStream.listen((snapshot) {
-    snapshot.docs.forEach((doc) {
-      print(doc.data());
+    voucherStream.listen((snapshot) {
+      snapshot.docs.forEach((doc) {
+        print(doc.data());
+      });
     });
-  });
 
-  return voucherStream;
-}
+    return voucherStream;
+  }
 
+  Future<Stream<QuerySnapshot>> getVoucherStreamForUser(String userID) async {
+    try {
+      // Querying userPurchases collection to find all purchases made by the user
+      QuerySnapshot userPurchaseSnapshot =
+          await purchases.where('userID', isEqualTo: userID).get();
 
-Future<Stream<QuerySnapshot>> getVoucherStreamForUser(String userID) async {
+      // Extracting voucher IDs from user purchases
+      List<String> voucherIDs = [];
+      userPurchaseSnapshot.docs.forEach((doc) {
+        voucherIDs.add(doc.get('voucherID') as String);
+        print(voucherIDs);
+      });
+
+      // Now that you have the voucher IDs, you can call getVoucherStreamByIDs
+      return getVoucherStreamByIDs(voucherIDs);
+    } catch (e) {
+      // Handle error
+      print('Error fetching user purchase data: $e');
+      return Stream.empty(); // Return an empty stream in case of error
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getUserPurchasesWithVoucherInfo(String userID) async {
   try {
-    // Querying userPurchases collection to find all purchases made by the user
-    QuerySnapshot userPurchaseSnapshot = await purchases
-        .where('userID', isEqualTo: userID)
-        .get();
+    // Query purchases collection to find the documents with the given userID
+    QuerySnapshot purchaseSnapshot = await purchases.where('userID', isEqualTo: userID).get();
+    if (purchaseSnapshot.docs.isEmpty) {
+      return []; // No purchases found for the given userID
+    }
+    // List to store user purchases with voucher info
+    List<Map<String, dynamic>> userPurchasesWithVoucherInfo = [];
 
-    // Extracting voucher IDs from user purchases
-    List<String> voucherIDs = [];
-    userPurchaseSnapshot.docs.forEach((doc) {
-      voucherIDs.add(doc.get('voucherID') as String);
-      print(voucherIDs);
-    });
+    // Loop through each user purchase document
+    for (DocumentSnapshot purchaseDoc in purchaseSnapshot.docs) {
+      String voucherID = purchaseDoc['voucherID'] as String;
+      bool isRedeem = purchaseDoc['isRedeem'] as bool;
 
-    // Now that you have the voucher IDs, you can call getVoucherStreamByIDs
-    return getVoucherStreamByIDs(voucherIDs);
+      // Query vouchers collection to get the voucher info
+      DocumentSnapshot voucherSnapshot = await vouchers.doc(voucherID).get();
+      if (!voucherSnapshot.exists) {
+        continue; // Skip if no voucher found with the given voucherID
+      }
+
+      // Extract voucher info
+      Map<String, dynamic> voucherInfo = voucherSnapshot.data() as Map<String, dynamic>;
+
+      // Add user purchase with voucher info to the list only if isRedeem is false
+      if (!isRedeem) {
+        userPurchasesWithVoucherInfo.add({
+          'userID': userID,
+          'voucherID': voucherID,
+          'voucherInfo': voucherInfo,
+          'isRedeem': isRedeem,
+        });
+      }
+    }
+    // Return the list of user purchases with voucher info
+    return userPurchasesWithVoucherInfo;
   } catch (e) {
-    // Handle error
-    print('Error fetching user purchase data: $e');
-    return Stream.empty(); // Return an empty stream in case of error
+    print('Error retrieving user purchases with voucher info: $e');
+    return [];
   }
 }
 
-  
- 
+
+Future<Map<String, List<Map<String, dynamic>>>> getVouchersByCategory(String category) async {
+  try {
+    // Retrieve user purchases with voucher info for the given category
+    List<Map<String, dynamic>> userPurchasesWithVoucherInfo = await getUserPurchasesWithVoucherInfo(category);
+
+    // Categorize vouchers by type
+    Map<String, List<Map<String, dynamic>>> categorizedVouchers = {};
+    for (var purchaseInfo in userPurchasesWithVoucherInfo) {
+      String voucherType = purchaseInfo['voucherInfo']['voucherType'] as String;
+      categorizedVouchers.putIfAbsent(voucherType, () => []);
+      categorizedVouchers[voucherType]!.add(purchaseInfo['voucherInfo']);
+    }
+
+    // Return the categorized vouchers
+    return categorizedVouchers;
+  } catch (e) {
+    // Handle error
+    print('Error retrieving vouchers by category: $e');
+    return {};
+  }
 }
 
+   // Redeem VOucher by docID
+  Future<void> RedeemVoucher(String docID) {
+    return purchases.doc(docID).update({
+      'isRedeem': true,
+      'timestamp': Timestamp.now(),
+    });
+  }
 
-
-
-
-
+}
---
+++ lib/views/vouhcer_item.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:project/utils/format_string.dart';
+
+class VoucherItem extends StatefulWidget {
+  final Map<String, dynamic> voucherInfo;
+
+  const VoucherItem({Key? key, required this.voucherInfo}) : super(key: key);
+
+  @override
+  _VoucherItemState createState() => _VoucherItemState();
+}
+
+class _VoucherItemState extends State<VoucherItem> {
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  ClipRRect(
+                    child: Image.network(
+                      widget.voucherInfo['imageUrl'], // Assuming 'image' is the URL of the voucher image
+                      height: double.infinity,
+                      width: double.infinity,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                      ),
+                      onPressed: () {
+                        if (Navigator.canPop(context)) {
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Text(
+                widget.voucherInfo['name'], // Assuming 'name' is the name of the voucher
+                style: const TextStyle(fontSize: 32),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Divider(
+                color: Colors.grey,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Valid Until ${FormatUtils.formatDate(widget.voucherInfo['dueDate'])}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      "Terms & Conditions",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  Text(
+                    widget.voucherInfo['termsCondition'], 
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: _isChecked,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _isChecked = newValue ?? false;
+                      });
+                    },
+                  ),
+                  Flexible(
+                    child: Text(
+                      'I agree to all the terms and conditions of usage.',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: ElevatedButton(
+                onPressed: _isChecked
+                    ? () {
+                        Navigator.pop(context);
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  backgroundColor: Colors.deepOrange,
+                  padding: const EdgeInsets.all(10),
+                ),
+                child: Text(
+                  "Redeem",
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
