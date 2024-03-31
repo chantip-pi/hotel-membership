fe476344eb2519b265d25a94491f0dafecec59e9
himechi <chantip.pi@ku.th>
2024-03-31 15:33:27 +0700

    complete purchase voucher ui

--- lib/views/user/shop/purchase_voucher.dart
+++ lib/views/user/shop/purchase_voucher.dart
@@ -1,14 +1,13 @@
-
 import 'package:flutter/material.dart';
 import 'package:project/services/voucher_service.dart';
-import 'package:project/utils/theme.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/utils/format_string.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project/models/cart.dart';
 import 'package:provider/provider.dart';
 
 class PurchaseVoucher extends StatefulWidget {
   final String voucherID;
+
   const PurchaseVoucher({Key? key, required this.voucherID}) : super(key: key);
 
   @override
@@ -39,26 +38,187 @@ class _PurchaseVoucherState extends State<PurchaseVoucher> {
     });
   }
 
-  Widget _buildAmountButton(
-    IconData iconData, Function() onPressed) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        backgroundColor: Colors.white,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Detail",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
       ),
-      child: Container(
-        width: screenWidth * 0.05,
-        height: screenWidth * 0.05,
-        child: Center(child: Icon(iconData, color: Colors.black)),
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: _voucher,
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || !snapshot.data!.exists) {
+            return Text(snapshot.toString());
+          } else {
+            final voucherData = snapshot.data!.data() as Map<String, dynamic>;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildImageSection(voucherData),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        buildNameSection(voucherData),
+                        buildDetailsSection(voucherData),
+                      ],
+                    ),
+                  ),
+                ),
+                buildAmountControlSection(),
+                buildToCartButton(voucherData),
+              ],
+            );
+          }
+        },
       ),
     );
   }
 
-  Widget _buildToCartButton(Map<String, dynamic> voucherData) {
+  Widget buildImageSection(Map<String, dynamic> voucherData) {
+    return ClipRRect(
+      child: Image.network(
+        voucherData['imageUrl'],
+        height: 200,
+        width: double.infinity,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Widget buildNameSection(Map<String, dynamic> voucherData) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Center(
+        child: Text(
+          voucherData['name'],
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+
+  Widget buildDetailsSection(Map<String, dynamic> voucherData) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Valid Until ${FormatUtils.formatDate(voucherData['dueDate'])}",
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+          const Text(
+            "Terms & Conditions",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: buildBulletPoints(voucherData['termsCondition']),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBulletPoints(String termsCondition) {
+    final bulletPoints = termsCondition.split('\\n');
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: bulletPoints.map((bulletPoint) {
+        return Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '• ',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  bulletPoint.trim(),
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+
+  Widget buildAmountControlSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisAlignment:
+            MainAxisAlignment.end, // Align the row to the end (right)
+        children: [
+          IconButton(
+            onPressed: decrement,
+            icon: const Icon(
+              Icons.remove,
+              color: Colors.white,
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(10),
+              backgroundColor: Colors.black,
+            ),
+          ),
+          SizedBox(
+            width: 80,
+            child: Center(
+              child: Text(
+                count.toString(),
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: increment,
+            icon: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(10),
+              backgroundColor: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildToCartButton(Map<String, dynamic> voucherData) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       child: Row(
         children: [
           Expanded(
@@ -78,13 +238,13 @@ class _PurchaseVoucherState extends State<PurchaseVoucher> {
                 final cart = Provider.of<Cart>(context, listen: false);
                 cart.addToCart(
                   widget.voucherID,
-                  count
-                 );
-                 Navigator.pop(context);
+                  count,
+                );
+                Navigator.pop(context);
               },
-              child:  Center(
+              child: Center(
                 child: Text(
-                  "Add to cart ${(count * voucherData['points'])} Points",
+                  "Add to Cart ${(count * voucherData['points'])} Points",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
@@ -95,104 +255,6 @@ class _PurchaseVoucherState extends State<PurchaseVoucher> {
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      backgroundColor: AppTheme.backgroundColor,
-      appBar: AppBar(
-        title: const Text('Purchase Voucher'),
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: _voucher,
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (!snapshot.hasData || !snapshot.data!.exists) {
-            return Text(snapshot.toString());
-          } else {
-            final voucherData = snapshot.data!.data() as Map<String, dynamic>;
-            return Column(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: ClipRRect(
-                    child: Image.network(
-                      voucherData[
-                          'imageUrl'], // Assuming 'image' is the URL of the voucher image
-                      height: double.infinity,
-                      width: double.infinity,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.only(top: screenHeight * 0.01)),
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Text(voucherData['name']),
-                        ),
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Text(
-                              FormatUtils.formatDate(voucherData['timestamp'])),
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: screenHeight * 0.02)),
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            'Terms & Conditions',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(top: screenHeight * 0.02)),
-                        Text(voucherData['termsCondition']),
-                        Padding(
-                            padding: EdgeInsets.only(top: screenHeight * 0.03)),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              _buildAmountButton(Icons.remove, decrement),
-                              Padding(
-                                  padding: EdgeInsets.only(
-                                      left: screenWidth * 0.05)),
-                              Text(count.toString()),
-                              Padding(
-                                  padding: EdgeInsets.only(
-                                      left: screenWidth * 0.05)),
-                              _buildAmountButton(Icons.add, increment),
-                            ],
-                          ),
-                        ),
-                        _buildToCartButton(voucherData),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }
-        },
       ),
     );
   }
