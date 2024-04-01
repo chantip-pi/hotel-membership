d5f0c3b9de083de4e4e74db3a1cd6b5b27a39b9c
wannga <wanwalaichen@gmail.com>
2024-04-01 09:12:19 +0700

    modify and clean unused code in my_voucher page

--- lib/views/user/my_voucher.dart
+++ lib/views/user/my_voucher.dart
@@ -1,11 +1,7 @@
-import 'package:async/async.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/services/user_purchase.dart';
-import 'package:project/services/user_service.dart';
-import 'package:project/services/voucher_service.dart';
 import 'package:project/utils/format_string.dart';
 import 'package:project/utils/theme.dart';
 import 'package:project/views/user/vouhcer_item.dart';
@@ -21,8 +17,7 @@ class _MyVoucherState extends State<MyVoucher> {
   late String _currentUserID;
   late Future<List<Map<String, dynamic>>> _userPurchasesWithVoucherInfo;
 
-  String voucherCategory = 'Discount';
-  int _selectedIndex = 0;
+  String voucherCategory = 'All';
 
   @override
   void initState() {
@@ -53,7 +48,13 @@ class _MyVoucherState extends State<MyVoucher> {
         future: _userPurchasesWithVoucherInfo,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
+            return const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  AppTheme.primaryColor,
+                ),
+              ),
+            );
           }
           if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
@@ -64,30 +65,8 @@ class _MyVoucherState extends State<MyVoucher> {
           List<Map<String, dynamic>> userPurchasesWithVoucherInfo =
               snapshot.data!;    
           List<Widget> voucherWidgets =
-              userPurchasesWithVoucherInfo.map((purchase) {
-            var voucherInfo = purchase['voucherInfo'] as Map<String, dynamic>;
-            return GestureDetector(
-              onTap: () {
-                // setState(() {
-                //   _selectedIndex;
-                // });
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => VoucherItem(
-                      voucherInfo: voucherInfo,
-                      purchaseID: purchase['purchaseID']
-                    )
-                  ),
-                );
-              },
-              child: _buildListTileByCase(voucherInfo, voucherCategory),
-            );
-          }).toList();
+              _buildVoucherWidgets(userPurchasesWithVoucherInfo);
 
-          // return ListView(
-          //   children: voucherWidgets,
-          // );
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -118,6 +97,26 @@ class _MyVoucherState extends State<MyVoucher> {
       ),
     );
   }
+  
+  List<Widget> _buildVoucherWidgets(List<Map<String, dynamic>> userPurchasesWithVoucherInfo) {
+    return userPurchasesWithVoucherInfo.map((purchase) {
+      var voucherInfo = purchase['voucherInfo'] as Map<String, dynamic>;
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => VoucherItem(
+                voucherInfo: voucherInfo,
+                purchaseID: purchase['purchaseID']
+              )
+            ),
+          );
+        },
+        child: _buildListTileByCase(voucherInfo, voucherCategory),
+      );
+    }).toList();
+  }
 
   Widget _buildHorizontalList() {
     List<String> icons = [
@@ -138,7 +137,15 @@ class _MyVoucherState extends State<MyVoucher> {
           return GestureDetector(
           onTap: () {
             setState(() {
-              _selectedIndex = index;
+              if (index == 0) {
+                voucherCategory = 'All';
+              } else if (index == 1) {
+                voucherCategory = 'Discount';
+              } else if (index == 2) {
+                voucherCategory = 'Cash';
+              } else {
+                voucherCategory = 'Gift';
+              }
               _textChanger(labels[index]);
             });
           },
@@ -196,16 +203,9 @@ class _MyVoucherState extends State<MyVoucher> {
   }
 
   Widget _buildListTileByCase(Map<String, dynamic> voucherInfo, String voucherCategory) {
-    // Map<String, dynamic> usingInfo = voucherInfo;
-    // Stream<QuerySnapshot> useInfo = usingInfo as Stream<QuerySnapshot>;
-    // VoucherService voucherService = VoucherService();
-    // Stream<QuerySnapshot> voucherStream = voucherService.getUserVoucherStreamByCategory(useInfo, voucherCategory);
     if (voucherCategory == 'All') {
       return _buildListTile(voucherInfo);
     } else {
-    // UserPurchaseService userPurchaseService = UserPurchaseService();
-    // Future<Map<String, List<Map<String, dynamic>>>> voucherStream =
-    //     userPurchaseService.getVouchersByCategory(voucherCategory);
       UserPurchaseService userPurchaseService = UserPurchaseService();
       Future<Map<String, List<Map<String, dynamic>>>> voucherFuture =
           userPurchaseService.getVouchersByCategory(voucherCategory, _currentUserID);
@@ -214,27 +214,26 @@ class _MyVoucherState extends State<MyVoucher> {
         future: voucherFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
+            return const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  AppTheme.primaryColor,
+                ),
+              ),
+            );
           }
           if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           }
           if (!snapshot.hasData || snapshot.data == null) {
-            return Text('No vouchers available');
+            return const Text('No vouchers available');
           }
 
           Map<String, List<Map<String, dynamic>>> voucherData = snapshot.data!;
-          // List<Map<String, dynamic>> vouchers =
-          //     voucherData[voucherCategory] ?? [];
           List<Map<String, dynamic>> vouchers = [];
           voucherData.forEach((key, value) {
             vouchers.addAll(value);
           });
-          
-          print(voucherFuture);
-          print('data $voucherData');
-          print('list $vouchers');
-          print('category $voucherCategory');
 
           return SizedBox(
             height: MediaQuery.of(context).size.height * 0.5,
@@ -246,50 +245,9 @@ class _MyVoucherState extends State<MyVoucher> {
               },
             ),
           );
-          // for (Map<String, dynamic> i in vouchers) {
-          //   usingInfo = i;
-          // }
-          // return _buildListTile(usingInfo);
         },
       );
     }
-
-    // if (_selectedIndex == 0) {
-    //   return _buildListTile(voucherInfo);
-    // } else {
-    //   usingInfo = voucherStream as Map<String, dynamic>;
-    //   return _buildListTile(usingInfo);
-    // }
-
-    // return StreamBuilder(
-    //   stream: voucherStream,
-    //   builder: (context, snapshot) {
-    //     if (snapshot.connectionState == ConnectionState.waiting) {
-    //       return CircularProgressIndicator(); // Or any other loading indicator
-    //     }
-    //     if (snapshot.hasError) {
-    //       return Text('Error: ${snapshot.error}');
-    //     }
-    //     if (!snapshot.hasData) {
-    //       return Text('No data available');
-    //     }
-
-    //     List<DocumentSnapshot> voucherDocuments = snapshot.data!.docs;
-    //     // Filter vouchers based on voucherType
-    //     List<Map<String, dynamic>> filteredVouchers = voucherDocuments
-    //         .map((doc) => doc.data() as Map<String, dynamic>)
-    //         .where((voucher) => voucher['voucherType'] == voucherCategory)
-    //         .toList();
-
-    //     return ListView.builder(
-    //       itemCount: filteredVouchers.length,
-    //       itemBuilder: (context, index) {
-    //         Map<String, dynamic> usingInfo = filteredVouchers[index];
-    //         return _buildListTile(usingInfo);
-    //       },
-    //     );
-    //   },
-    // );
   }
   
   Widget _buildListTile(Map<String, dynamic> usingInfo) {
